722fc56cba35399fc9f903f2ebe5a6dcf0febebb
Nioto <antoine.guillemoto@gmail.com>
2017-11-16 21:00:52 +0000

    feat(annonce) add timestamps

--- mongo/schemas/annonce.js
+++ mongo/schemas/annonce.js
@@ -8,6 +8,9 @@ const schema = new Schema({
     name: { type: String }, 
     creator: { type: Schema.Types.ObjectId, ref: ShemaNames.USER },
     sport: { type: Schema.Types.ObjectId, ref: ShemaNames.SPORT }
+},
+{
+    timestamps: true
 });
 
 module.exports = {
